35651a2dc50ca14dd3d34a1bd84dead98cb7a84d
Sun Huang <sun_huang@charder.com.tw>
2023-02-07 22:55:55 +0800

    20230207 final

--- iOSInterview/view/CustomTabBarController.swift
+++ iOSInterview/view/CustomTabBarController.swift
@@ -4,6 +4,8 @@ import UIKit
 
 class CustomTabBarController: UITabBarController {
     
+    var barview : UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,7 +18,17 @@ class CustomTabBarController: UITabBarController {
         tabBar.frame.size.width = view.bounds.width - 48
         tabBar.frame.origin.x = 24
         tabBar.frame.origin.y = view.bounds.height - 100
-
+        barview = UIView()
+        barview.frame = tabBar.frame
+        barview.layer.cornerRadius = 25
+        barview.backgroundColor = .gray
+        barview.layer.shadowColor = UIColor.black.cgColor
+        barview.layer.shadowOffset = CGSize(width: 2, height: 2)
+        barview.layer.shadowOpacity = 0.2
+        barview.layer.shadowRadius = 5
+        barview.layer.shadowOpacity = 0.5
+        barview.layer.masksToBounds = false
+        view.insertSubview(barview, belowSubview: self.tabBar)
         tabBar.layer.cornerRadius = 25
         tabBar.clipsToBounds = true
         tabBar.tintColor = UIColor.orange01
