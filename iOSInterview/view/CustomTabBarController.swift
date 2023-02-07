611620b1df3298f3f81229d88f85ecc170602a41
Sun Huang <sun_huang@charder.com.tw>
2023-02-07 23:05:44 +0800

    20230207 final fix2

--- iOSInterview/view/CustomTabBarController.swift
+++ iOSInterview/view/CustomTabBarController.swift
@@ -17,17 +17,20 @@ class CustomTabBarController: UITabBarController {
         tabBar.frame.size.width = view.bounds.width - 48
         tabBar.frame.origin.x = 24
         tabBar.frame.origin.y = view.bounds.height - 100
-        barview = UIView()
-        barview.frame = tabBar.frame
-        barview.layer.cornerRadius = 25
-        barview.backgroundColor = .gray
-        barview.layer.shadowColor = UIColor.black.cgColor
-        barview.layer.shadowOffset = CGSize(width: 2, height: 2)
-        barview.layer.shadowOpacity = 0.2
-        barview.layer.shadowRadius = 5
-        barview.layer.shadowOpacity = 0.5
-        barview.layer.masksToBounds = false
-        view.insertSubview(barview, belowSubview: self.tabBar)
+        if barview == nil {
+            barview = UIView()
+            barview.frame = tabBar.frame
+            barview.layer.cornerRadius = 25
+            barview.backgroundColor = .gray
+            barview.layer.shadowColor = UIColor.black.cgColor
+            barview.layer.shadowOffset = CGSize(width: 2, height: 2)
+            barview.layer.shadowOpacity = 0.2
+            barview.layer.shadowRadius = 5
+            barview.layer.shadowOpacity = 0.5
+            barview.layer.masksToBounds = false
+            view.insertSubview(barview, belowSubview: self.tabBar)
+        }
+        
         tabBar.layer.cornerRadius = 25
         tabBar.clipsToBounds = true
         tabBar.tintColor = UIColor.orange01
